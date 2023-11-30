11459bc5b8c419b4285ea6840b76f51cbd7c94ec
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-11-30 08:03:21 +0100

    BugFix: Import error module not found solved

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,11 +1,11 @@
-import _db from "./_db";
+import _db from "./_db.js";
 
 // Resolvers define how to fetch the types defined in your schema.
-const resolver = {
+const resolvers = {
 	Query: {
 		games: () => _db.games,
 		reviews: () => _db.reviews,
 		authors: () => _db.authors,
 	},
 };
-export default resolver;
+export default resolvers;
